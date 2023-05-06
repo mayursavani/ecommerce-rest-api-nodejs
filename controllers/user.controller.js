d1cc279e0baca87dcd2d51ee0da4610120add7c5
mayursvani <mayursavani99@gmail.com>
2023-05-06 18:45:24 +0530

    added login api

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,16 +1,40 @@
 const { default: mongoose } = require("mongoose");
 const User = require("../models/user.model");
 const asyncHandler = require("express-async-handler");
+const jwtToken = require("../config/jwttoken");
 
 const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
-  const isFoundUser = await mongoose.model("User").findOne({ email: email });
+  const isFoundUser = await User.findOne({ email: email });
   if (!isFoundUser) {
     const newUser = await User.create(req.body);
     res.json(newUser);
   } else {
-    throw new Error('User Already Exist')
+    throw new Error("User Already Exist");
   }
 });
 
-module.exports = { createUser };
+const validateLoginUserController = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const findUser = await User.findOne({ email });
+  if (!findUser) {
+    throw new Error(
+      "User not registered with given email please try to sign up"
+    );
+  }
+  const isValidUser = await findUser.isPasswordMatched(password);
+  if (!isValidUser) {
+    throw new Error("Please check your password or try reset password");
+  } else {
+    res.send({
+      _id: findUser?._id,
+      firstName: findUser?.firstName,
+      lastName: findUser?.lastName,
+      email: findUser?.email,
+      mobile: findUser?.mobile,
+      token: jwtToken.generateToken(findUser?._id),
+    });
+  }
+});
+
+module.exports = { createUser, validateLoginUserController };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createUser } = require("../controllers/user.controller");
+const {
+  createUser,
+  validateLoginUserController,
+} = require("../controllers/user.controller");
 
 router.post("/register", createUser);
+router.post("/login", validateLoginUserController);
 
 module.exports = router;
