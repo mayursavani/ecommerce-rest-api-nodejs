b67e017809db21abeece88176d107ca2f707e22f
mayursvani <mayursavani99@gmail.com>
2023-05-08 13:21:26 +0530

    added user crud

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -32,9 +32,67 @@ const validateLoginUserController = asyncHandler(async (req, res) => {
       lastName: findUser?.lastName,
       email: findUser?.email,
       mobile: findUser?.mobile,
+      role: findUser?.role,
       token: jwtToken.generateToken(findUser?._id),
     });
   }
 });
 
-module.exports = { createUser, validateLoginUserController };
+//all users
+const getAllUser = asyncHandler(async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    throw new Error(error);
+  }
+  const users = await User.find();
+});
+
+// get single user
+
+const getUser = asyncHandler(async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    res.json(user);
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+
+//delete a single user
+
+const deleteUser = asyncHandler(async (req, res) => {
+  try {
+    const deleteUser = await User.findByIdAndDelete(req.params.id);
+    res.json(deleteUser);
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+
+//update a user
+const updateUser = asyncHandler(async (req, res) => {
+  try {
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        firstName: req?.body?.firstName,
+        lastName: req?.body?.lastName,
+      },
+      { new: true }
+    );
+    res.json(updateUser);
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+
+module.exports = {
+  createUser,
+  validateLoginUserController,
+  getAllUser,
+  getUser,
+  deleteUser,
+  updateUser,
+};
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,9 +3,17 @@ const router = express.Router();
 const {
   createUser,
   validateLoginUserController,
+  getAllUser,
+  getUser,
+  deleteUser,
+  updateUser,
 } = require("../controllers/user.controller");
 
 router.post("/register", createUser);
 router.post("/login", validateLoginUserController);
+router.get("/all-users", getAllUser);
+router.get("/:id", getUser);
+router.delete("/:id", deleteUser);
+router.put("/:id", updateUser);
 
 module.exports = router;
